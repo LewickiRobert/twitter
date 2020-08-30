8b25a6982a9b383fdb33b332a8bb714cdee256fd
Robert Lewicki <lewicki.robert@onet.pl>
2020-08-30 13:36:20 +0200

    add articale

--- src/main/java/pl/sda/twitter/peristance/dao/ArticleDao.java
+++ src/main/java/pl/sda/twitter/peristance/dao/ArticleDao.java
@@ -4,15 +4,28 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import pl.sda.twitter.peristance.HibernateUtil;
 import pl.sda.twitter.peristance.entities.TbArticle;
+import pl.sda.twitter.peristance.entities.TbUser;
 
 import java.util.List;
 
 public class ArticleDao {
 
-    public List getArticles(){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+    public void addNewArticle(TbUser tbUser, String content) {
+        try (final Session session = HibernateUtil.getSessionFactory().openSession()) {
             session.beginTransaction();
-            final Query q = session.createQuery("SELECT o from "+ TbArticle.class.getName() +" o");
+
+            final TbArticle tbArticle = new TbArticle();
+            tbArticle.setContent(content);
+            tbArticle.setUser(tbUser);
+            session.save(tbArticle);
+            session.getTransaction().commit();
+        }
+    }
+
+    public List getArticles() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            final Query q = session.createQuery("SELECT o from " + TbArticle.class.getName() + " o");
             session.getTransaction().commit();
             return q.getResultList();
 
--- src/main/java/pl/sda/twitter/services/ArticleService.java
+++ src/main/java/pl/sda/twitter/services/ArticleService.java
@@ -1,14 +1,23 @@
 package pl.sda.twitter.services;
 
 import pl.sda.twitter.peristance.dao.ArticleDao;
+import pl.sda.twitter.peristance.entities.TbArticle;
+import pl.sda.twitter.peristance.entities.TbUser;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ArticleService {
 
-    private ArticleDao articleDao = new ArticleDao();
+    private final ArticleDao articleDao = new ArticleDao();
 
-    public List getArticles(){
-        return articleDao.getArticles();
+    public void addArticle(TbUser tbUser, String content) {
+        articleDao.addNewArticle(tbUser, content);
+    }
+
+    public List getArticles() {
+        final List<TbArticle> articles = articleDao.getArticles();
+        articles.sort(Comparator.comparing(TbArticle::getId).reversed());
+        return articles;
     }
 }
